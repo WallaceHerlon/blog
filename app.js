451048506f55331f08ae92528815b775273f538a
Wallace Herlon <wherlon@hotmail.com>
2022-06-13 21:21:28 -0300

    Arquivo estáticos

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
+const path = require('path')
 //const mongoose = require('mongoose')
 
 // Configurações
@@ -11,10 +12,12 @@ const admin = require('./routes/admin')
 //app.use(bodyParser.urlencoded({extended: true}))
 //app.use(bodyParser.json())
 //Handlebars
-app.engine('handlebars',handlebars.engine({defaulyLayout:'main'}))
+app.engine('handlebars', handlebars.engine({ defaulyLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //Mongoose
 
+//Public
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Rotas
 app.use('/admin', admin)

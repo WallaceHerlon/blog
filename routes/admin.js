e6e95d2a2df701544a1bbc39be9e68a5f5a602b3
Wallace Herlon <wherlon@hotmail.com>
2022-06-15 20:17:50 -0300

    Deletando postagens

--- routes/admin.js
+++ routes/admin.js
@@ -187,7 +187,16 @@ router.post('/postagem/edit', (req, res) => {
         req.flash('error_msg', 'Houve um erro ao salvar edição')
         res.redirect('/admin/postagens')
     })
+})
 
+router.get('/postagens/deletar/:id', (req, res) => {
+    Postagem.remove({_id: req.params.id}).then(() => {
+        req.flash('success_msg', 'Postagem deletada com sucesso!')
+        res.redirect('/admin/postagens')
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro interno')
+            res.redirect('/admin/postagens')
+    })    
 })
 
 module.exports = router

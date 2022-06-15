0ce64e0944b43fa3dfb3a6c23e24e464927c13d5
Wallace Herlon <wherlon@hotmail.com>
2022-06-14 23:59:54 -0300

    Formulário de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -94,8 +94,22 @@ router.post('/categorias/deletar', (req, res) => {
         res.redirect('/admin/categorias')
     }).catch((err) => {
         req.flash('error_msg', 'Houve um erro ao deletar categoria')
-            res.redirect('/admin/categorias')
+        res.redirect('/admin/categorias')
     })
+})
+
+router.get('/postagens', (req, res) => {
+    res.render('admin/postagens')
+})
+
+router.get('/postagens/add', (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render('admin/addpostagem', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao carregar o formulário')
+        res.redirect('/admin')
+    })
+
 })
 
 

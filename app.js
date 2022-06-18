d91e08b73078e167b6663bd668b95cb294bab5e7
Wallace Herlon <wherlon@hotmail.com>
2022-06-17 21:06:49 -0300

    Listagem de categorias

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const session = require('express-session')
 const flash = require('connect-flash')
 require('./models/Postagem')
 const Postagem = mongoose.model('postagens')
+require('./models/Categoria')
+const Categoria = mongoose.model('categorias')
 
 // Configurações
 
@@ -54,7 +56,7 @@ app.get('/', (req, res) => {
 app.get('/postagem/:slug', (req, res) => {
     Postagem.findOne({ slug: req.params.slug }).lean().then((postagem) => {
         if (postagem) {
-            res.render('postagem/index', {postagem: postagem})
+            res.render('postagem/index', { postagem: postagem })
         } else {
             req.flash('error_msg', 'Esta postagem não existe')
             res.redirect('/')
@@ -67,6 +69,34 @@ app.get('/postagem/:slug', (req, res) => {
 
 app.get('/404', (req, res) => {
     res.send('Erro 404!')
+})
+
+app.get('/categorias', (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render('categorias/index', { categorias: categorias })
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro interno ao lista as categorias')
+        res.redirect('/')
+    })
+})
+
+app.get('/categorias/:slug', (req, res) => {
+    Categoria.findOne({ slug: req.params.slug }).lean().then((categoria) => {
+        if (categoria) {
+            Postagem.find({ categoria: categoria._id }).lean().then((postagens) =>{
+                res.render('categorias/postagens', {postagens: postagens, categoria: categoria }) 
+            }).catch((err) => {
+                req.flash('error_msg', 'Houve um erro interno ao listar os posts!.')
+                res.redirect('/')
+            })
+        } else {
+            req.flash('error_msg', 'Esta categoria não existe')
+            res.redirect('/')
+        }
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro interno ao carregar a página desta categoria.')
+        res.redirect('/')
+    })
 })
 
 app.use('/admin', admin)

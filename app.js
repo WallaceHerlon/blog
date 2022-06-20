e0d914b7091bfb3d828e3e0b43522dd97c4e6cff
Wallace Herlon <wherlon@hotmail.com>
2022-06-20 12:02:32 -0300

    Configurando o Passport
    Passport, para cuidar da autenticação da aplicação.

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const Postagem = mongoose.model('postagens')
 require('./models/Categoria')
 const Categoria = mongoose.model('categorias')
 const usuarios = require('./routes/usuario')
+const passport = require('passport')
+require('./config/auth')(passport)
+
 
 // Configurações
 
@@ -21,6 +24,8 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 //Middleware
 app.use((req, res, next) => {

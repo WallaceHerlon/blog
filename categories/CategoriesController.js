b815944fc92e0388e82a6ed3456390e67d40e342
Wallace Herlon <wherlon@hotmail.com>
2022-07-18 18:13:03 -0300

    Formulário de edição

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -15,7 +15,7 @@ router.post('/categories/save', (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect('/')
+            res.redirect('/admin/categories')
         })
 
     } else {
@@ -48,6 +48,21 @@ router.post('/categories/delete', (req, res) => {
     }
 })
 
+router.get('/admin/categories/edit/:id', (req, res) => {
+    var id = req.params.id
+    if(isNaN(id)){
+        res.redirect('/admin/categories')
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined) {
+            res.render('admin/categories/edit', { category: category})   
+        } else {
+            res.redirect('/admin/categories')
+        }
+    }).catch( erro => {
+        res.redirect('admin/categories')
+    })
+})
 
 module.exports = router
 

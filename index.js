dd938799bf6dd772806fab17fa6415167ab962b5
Wallace Herlon <wherlon@hotmail.com>
2022-07-19 22:06:28 -0300

    Página de leitura de artigo

--- index.js
+++ index.js
@@ -29,13 +29,31 @@ connection.authenticate()
 
 app.use('/', categoriesController)
 app.use('/', articlesController)
-    
+
 app.get('/', (req, res) => {
     Article.findAll().then(articles => {
-       res.render('index', { articles: articles }) 
+        res.render('index', { articles: articles })
     })
-    
 })
+
+app.get('/:slug', (req, res) => {
+    var slug = req.params.slug
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if (article != undefined) {
+            res.render('article', { article: article})
+        } else {
+            res.redirect('/')
+        }
+    }).catch(err => {
+        res.redirect('/')
+    })
+})
+
+
 
 app.listen(8080, () => {
     console.log('o servidor está rodando!')

657d8a8efdc710a4886d4c8a3f889c5f35208780
Wallace Herlon <wherlon@hotmail.com>
2022-07-25 20:38:29 -0300

    Express sessions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const connection = require('./database/database')
+const session = require('express-session')
 
 const categoriesController = require('./categories/CategoriesController')
 const articlesController = require('./articles/ArticlesController')
@@ -13,6 +14,11 @@ const User = require('./user/User')
 
 //View engine
 app.set('view engine', 'ejs')
+
+//Session
+app.use(session({
+    secret: 'qualquercoisa', cookie: { maxAge: 30000 }
+}))
 
 //Static
 app.use(express.static('public'))
